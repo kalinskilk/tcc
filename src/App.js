360adfb775c95c5cf3a10218a78790ef9b031dae
kalinski <lukkas_megafox@hotmail.com>
2022-10-09 14:35:21 -0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useWebSocket from "react-use-websocket";
+import { useState } from "react";
 
 function App() {
+  const [vagas, setVagas] = useState([]);
+  const { lastJsonMessage, sendMessage } = useWebSocket("ws://localhost:8080", {
+    onOpen: () => console.log(`Connected to App WS`),
+    onMessage: () => {
+      if (lastJsonMessage) {
+        setVagas(lastJsonMessage);
+        console.log(lastJsonMessage);
+      }
+    },
+    queryParams: { token: "123456" },
+    onError: (event) => {
+      console.error(event);
+    },
+    shouldReconnect: (closeEvent) => true,
+    reconnectInterval: 3000,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div class="card mt-2">
+        <div class="card-body">
+          <div class="row">
+            {vagas?.map((el) => {
+              const statusColor = el.status
+                ? "border-danger bg-danger"
+                : "border-success bg-success";
+
+              return (
+                <div
+                  class={`p-2 col-sm-2 card card-vagas ${statusColor} text-center`}
+                  key={el.id}
+                >
+                  <b>Vaga Nº{el.id}</b>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+      crossorigin="anonymous"
+    ></link>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+      crossorigin="anonymous"
+    ></script>
     <App />
   </React.StrictMode>
 );
